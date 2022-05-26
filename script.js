027dd292c7ef72abf69adaecc34af5bbff2dbbba
JoleneMel <jolenemelanson33@gmail.com>
2022-05-25 20:58:35 -0400

    http fetch

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ fetch('https://jsonplaceholder.typicode.com/users')
     for (var i = 0; i < userData.length; i++) {
         //creating rows with template literals by appending it
         var row = `<tr>
-                        <td>${userData[i].name}</td>
-                        <td>${userData[i].website}</td>
-                        <td>${userData[i].email}</td>
-                        <td>${userData[i].address.street}  ${userData[i].address.suite} ${userData[i].address.city}  ${userData[i].address.zipcode}</td>
+                        <td> ${userData[i].name} </td>
+                        <td> ${userData[i].website} </td>
+                        <td> ${userData[i].email} </td>
+                        <td> ${userData[i].address.street}  ${userData[i].address.suite} ${userData[i].address.city}  ${userData[i].address.zipcode} </td>
                   </tr>`;
         //grabs variable and set innerhtml
         table.innerHTML += row;
@@ -36,19 +36,19 @@ fetch('https://jsonplaceholder.typicode.com/users')
 //     }
 //   }
 //this will take in the array which we will refer to as data,
-const buildTable = (data) => {
-    //we will query the table
-    var table = document.getElementById('myTable');
-    //while count is less then the length of the array
-    for (var i = 0; i < data.length; i++) {
-        //creating rows with template literals by appending it
-        var row = `<tr>
-                        <td>${data[i].name}</td>
-                        <td>${data[i].website}</td>
-                        <td>${data[i].email}</td>
-                        <td>${data[i].address}</td>
-                  </tr>`;
-        //grabs variable and set innerhtml
-        table.innerHTML += row;
-    }
-};
+// const buildTable = (data) => {
+//     //we will query the table
+//     var table = document.getElementById('myTable');
+//     //while count is less then the length of the array
+//     for (var i = 0; i < data.length; i++) {
+//         //creating rows with template literals by appending it
+//         var row = `<tr>
+//                         <td>${data[i].name}</td>
+//                         <td>${data[i].website}</td>
+//                         <td>${data[i].email}</td>
+//                         <td>${data[i].address}</td>
+//                   </tr>`;
+//         //grabs variable and set innerhtml
+//         table.innerHTML += row;
+//     }
+// };
--- script.ts
+++ script.ts
@@ -1,4 +1,4 @@
-
+//http request, and populating the table 
 type userData = {name:string, website:string, email:string, address:any}  
 let userData = [];
 fetch('https://jsonplaceholder.typicode.com/users')
@@ -14,10 +14,10 @@ fetch('https://jsonplaceholder.typicode.com/users')
     for (var i = 0; i < userData.length; i++){
         //creating rows with template literals by appending it
         var row = `<tr>
-                        <td>${userData[i].name}</td>
-                        <td>${userData[i].website}</td>
-                        <td>${userData[i].email}</td>
-                        <td>${userData[i].address.street}  ${userData[i].address.suite} ${userData[i].address.city}  ${userData[i].address.zipcode}</td>
+                        <td id = "name"> ${userData[i].name} </td>
+                        <td> ${userData[i].website} </td>
+                        <td> ${userData[i].email} </td>
+                        <td> ${userData[i].address.street}  ${userData[i].address.suite} ${userData[i].address.city}  ${userData[i].address.zipcode} </td>
                   </tr>`
                   //grabs variable and set innerhtml
         table!.innerHTML += row
@@ -39,20 +39,20 @@ console.log(userData)
 //     }
 //   }
 
- //this will take in the array which we will refer to as data,
- const buildTable = (data:userData[]) => {
-    //we will query the table
-    var table = document.getElementById('myTable')
-    //while count is less then the length of the array
-    for (var i = 0; i < data.length; i++){
-        //creating rows with template literals by appending it
-        var row = `<tr>
-                        <td>${data[i].name}</td>
-                        <td>${data[i].website}</td>
-                        <td>${data[i].email}</td>
-                        <td>${data[i].address}</td>
-                  </tr>`
-                  //grabs variable and set innerhtml
-        table!.innerHTML += row
-    }
-}
+//  //this will take in the array which we will refer to as data,
+//  const buildTable = (data:userData[]) => {
+//     //we will query the table
+//     var table = document.getElementById('myTable')
+//     //while count is less then the length of the array
+//     for (var i = 0; i < data.length; i++){
+//         //creating rows with template literals by appending it
+//         var row = `<tr>
+//                         <td>${data[i].name}</td>
+//                         <td>${data[i].website}</td>
+//                         <td>${data[i].email}</td>
+//                         <td>${data[i].address}</td>
+//                   </tr>`
+//                   //grabs variable and set innerhtml
+//         table!.innerHTML += row
+//     }
+// }
